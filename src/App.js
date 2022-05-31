077e44210a8886ce42e4f84a6ed14651943e0cac
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2022-05-31 14:23:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
 
 function App() {
+  const API_KEY = 'DPYN64R6MPM64Z3MW8W78EGEM'
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>WEATHER APP</h1>
       </header>
     </div>
   );
